fd5426645f0c63b1d4a6a8dfe2a1ed3523f0f1c2
lebohang_mdm <lebohangmdm@gmail.com>
2024-03-16 22:02:07 +0200

    ui complete

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,32 @@ function App() {
                 </div>
               </div>
             </div>
-            <div className="border border-red-500">cart</div>
+            <div className="border border-black p-4">
+              <div>
+                <p className="text-xl font-semibold uppercase mb-4">
+                  Order Summary
+                </p>
+                <div className="space-y-1">
+                  <div className="flex items-center justify-between">
+                    <p className=" text-gray-700 uppercase">Subtotal</p>
+                    <p className="font-semibold">$128</p>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <p className=" text-gray-700 uppercase">tax</p>
+                    <p className="font-semibold">$3</p>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <p className=" text-gray-700 uppercase">total</p>
+                    <p className="font-bold">$131</p>
+                  </div>
+                </div>
+                <div className="mt-4">
+                  <button className="py-2 px-4 w-full text-xl text-white bg-black">
+                    Checkout
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
